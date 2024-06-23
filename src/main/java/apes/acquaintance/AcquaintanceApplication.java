e217738c087ea0487fe447d17bcaf033146eec9e
bigth21 <taekhyeon.nam@gmail.com>
2024-06-23 19:52:35 +0900

    Init

---
+++ src/main/java/apes/acquaintance/AcquaintanceApplication.java
@@ -0,0 +1,13 @@
+package apes.acquaintance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcquaintanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcquaintanceApplication.class, args);
+    }
+
+}
